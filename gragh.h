ab4d649eaf741052ea554ee45cf699292e6b56c2
Abby <abby8050@gmail.com>
2019-10-15 20:57:41 +0800

    Add gragh header file

---
+++ gragh.h
@@ -0,0 +1,40 @@
+#ifndef __GRAPH__
+#define __GRAPH__
+
+#include "gluethread/glthread.h"
+
+#define TOPO_NAME_SIZE    32
+#define NODE_NAME_SIZE    16
+#define IF_NAME_SIZE      16
+#define MAX_INTF_PER_NODE 10
+
+
+typedef struct gragh_ gragh_t;
+typedef struct node_ node_t;
+typedef struct interface_ interface_t;
+typedef struct link_ link_t;
+
+typedef struct graph_ {
+    char topo_name[TOPO_NAME_SIZE];
+    glthread_t node_list;
+};
+
+struct node_ {
+    char node_name[NODE_NAME_SIZE];
+    interface_t *intf[MAX_INTF_PER_NODE];
+    glthread_t gragh_glue;
+};
+
+typedef struct interface_ {
+    char if_name[IF_NAME_SIZE];
+    struct link_ *link;
+    struct node_ *node;
+};
+
+struct link_ {
+    interface_t intf1;
+    interface_t intf2;
+    unsigned int cost;
+};
+
+#endif
