1a7219a2c6f465fbf38c128aaa537ca50bfebc22
Abby Wu <Abby.Wu@qct.io>
2019-10-16 11:43:13 +0800

    Add helper functions

--- gragh.h
+++ gragh.h
@@ -28,7 +28,7 @@ struct node_ {
 typedef struct interface_ {
     char if_name[IF_NAME_SIZE];
     struct link_ *link;
-    struct node_ *node;
+    struct node_ *att_node;
 };
 
 struct link_ {
@@ -36,5 +36,24 @@ struct link_ {
     interface_t intf2;
     unsigned int cost;
 };
+
+static inline node_t *get_nbr_node(interface_t *interface) {
+    link_t *link = interface->link;
+    if (&link->intf1 != interface) {
+        return (&link->intf1)->att_node;
+    } else {
+        return (&link->intf2)->att_node;
+    }
+}
+
+static inline int get_node_intf_available_slot(node_t *node){
+    for (int i = 0; i < MAX_INTF_PER_NODE; i++) {
+        if (node->intf[i]) {
+            continue;
+        }
+        return i;
+    }
+    return -1;
+}
 
 #endif
